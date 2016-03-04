10bcec19acad2ae16cd98056cbedc39dc5597378
shenzhenyuan <szywind@163.com>
2016-03-04 02:13:52 -0600

    add history pages

---
+++ MyCalendar/EventDetailViewController.swift
@@ -0,0 +1,41 @@
+//
+//  EventDetailViewController.swift
+//  MyCalendar
+//
+//  Created by ShenZhenyuan on 3/4/16.
+//  Copyright © 2016 ShenZhenyuan. All rights reserved.
+//
+
+import UIKit
+
+class EventDetailViewController: UIViewController {
+
+    var currentEvent: Event?
+    
+    @IBOutlet weak var detailLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        detailLabel.text = currentEvent?.date
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
