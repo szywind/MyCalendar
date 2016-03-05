f8e6785b10683d895080a172a9f99e0896bc8315
shenzhenyuan <szywind@163.com>
2016-03-05 02:20:06 -0600

    add persistence

--- MyCalendar/CreateEventViewController.swift
+++ MyCalendar/CreateEventViewController.swift
@@ -10,10 +10,77 @@ import UIKit
 
 class CreateEventViewController: UIViewController {
 
+    var event:Event?
+    
+    var eventList = [Event]()
+    
+    var date: NSDate?
+    
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var durationText: UITextField!
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var whereText: UITextField!
+    @IBOutlet weak var whatText: UITextField!
+    @IBOutlet weak var detailText: UITextField!
+    @IBOutlet weak var doneButton: UIBarButtonItem!
+    
+    func createEvent(){
+        
+        let time = datePicker.date
+        let duration = Int(durationText.text!)
+        let location = whereText.text
+        let title = whatText.text
+        let detail = detailText.text
+        event = Event(_title: title!, _date: date!, _time: time, _duration: duration!, _location: location!,_detail:  detail!)
+    }
+    
+//    func saveEvent(){
+//        var eventList = loadEvents()
+//        if (eventList != nil){
+//            print("number of events:", eventList!.count)
+//            eventList?.append(event!)
+//            saveEvents(eventList!)
+//        } else {
+//            // Create a new Course List
+//            //let eventList: EventCollection = EventCollection(items: [event!])
+//            var events = [Event]()
+//            events.append(event!)
+//            saveEvents(events)
+//        }
+//        
+//    }
+    
+    @IBAction func onFinished(sender: UIBarButtonItem) {
+        let alertController = UIAlertController(title: "Notification", message: "Do you want to finish creating event.", preferredStyle: .Alert)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel) { (action) in
+            // ...
+        }
+        alertController.addAction(cancelAction)
+        
+        let OKAction = UIAlertAction(title: "OK", style: .Default) { (action) in
+            // ...
+            self.createEvent()
+            self.eventList.append(self.event!)
+            self.saveEvents()
+            
+            
+            self.navigationController!.popToRootViewControllerAnimated(true)
+
+            //self.view.window!.rootViewController!.navigationController!.popViewControllerAnimated(true)  // for push/show
+            //self.dismissViewControllerAnimated(true, completion:nil) // for modal
+        }
+        alertController.addAction(OKAction)
+        
+        presentViewController(alertController, animated: true, completion: nil)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        if let savedEvents = loadEvents() {
+            eventList += savedEvents
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -38,4 +105,38 @@ class CreateEventViewController: UIViewController {
         return false
     }
 
+    @IBAction func onChangeValue(sender: UISlider) {
+        let intensity = Int(round(slider.value))
+        durationText.text = String(intensity)
+    }
+
+    @IBAction func onChangeText(sender: UITextField) {
+        if let intensity = Float(durationText.text!){
+            slider.value = intensity
+        }
+    }
+    
+    
+    
+    func saveEvents() {
+        let isSuccessfulSave = NSKeyedArchiver.archiveRootObject(eventList, toFile: Event.ArchiveURL.path!)
+        if !isSuccessfulSave {
+            print("Failed to save events...")
+        }
+    }
+    
+//    func loadEvents() -> [Event]? {
+//        
+//        if let temp = NSKeyedUnarchiver.unarchiveObjectWithFile(Event.ArchiveURL.path!) as? [Event]{
+//            return temp
+//        } else {
+//            return nil
+//        }
+//    }
+    
+    func loadEvents() -> [Event]? {
+        NSUserDefaults.standardUserDefaults().removeObjectForKey("events")
+        return NSKeyedUnarchiver.unarchiveObjectWithFile(Event.ArchiveURL.path!) as? [Event]
+    }
+    
 }
